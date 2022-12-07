13bb155d7931f68264ca08bfe1c90d2518ab52bc
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-07 12:28:17 +0430

    delete all the code from app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/Calculator" element={<Calculator />} />
+        </Routes>
+      </div>
+    );
+  }
 }
-
-export default App;
