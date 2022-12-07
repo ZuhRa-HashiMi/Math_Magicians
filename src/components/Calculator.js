192869f8fc084a81465ab09bad385f00b74866a6
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2022-12-07 20:46:01 +0430

    add logic for all button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
   const [state, setState] = useState({ total: 0, next: null, operation: null });
+
+  const handler = (e) => {
+    setState((state) => calculate(state, e.target.textContent));
+  };
 
   return (
     <section className="layout">
@@ -14,25 +19,25 @@ export default function Calculator() {
           {state.next}
         </div>
         <div className="keyboard">
-          <button type="button" className="gray">AC</button>
-          <button type="button" className="gray">+/-</button>
-          <button type="button" className="gray">%</button>
-          <button type="button" className="orange">÷</button>
-          <button type="button" className="gray">7</button>
-          <button type="button" className="gray">8</button>
-          <button type="button" className="gray">9</button>
-          <button type="button" className="orange">x</button>
-          <button type="button" className="gray">4</button>
-          <button type="button" className="gray">5</button>
-          <button type="button" className="gray">6</button>
-          <button type="button" className="orange">-</button>
-          <button type="button" className="gray">1</button>
-          <button type="button" className="gray">2</button>
-          <button type="button" className="gray">3</button>
-          <button type="button" className="orange">+</button>
-          <button type="button" className="gray" id="zero">0</button>
-          <button type="button" className="gray">.</button>
-          <button type="button" className="orange">=</button>
+          <button type="button" onClick={handler} className="gray">AC</button>
+          <button type="button" onClick={handler} className="gray">+/-</button>
+          <button type="button" onClick={handler} className="gray">%</button>
+          <button type="button" onClick={handler} className="orange">÷</button>
+          <button type="button" onClick={handler} className="gray">7</button>
+          <button type="button" onClick={handler} className="gray">8</button>
+          <button type="button" onClick={handler} className="gray">9</button>
+          <button type="button" onClick={handler} className="orange">x</button>
+          <button type="button" onClick={handler} className="gray">4</button>
+          <button type="button" onClick={handler} className="gray">5</button>
+          <button type="button" onClick={handler} className="gray">6</button>
+          <button type="button" onClick={handler} className="orange">-</button>
+          <button type="button" onClick={handler} className="gray">1</button>
+          <button type="button" onClick={handler} className="gray">2</button>
+          <button type="button" onClick={handler} className="gray">3</button>
+          <button type="button" onClick={handler} className="orange">+</button>
+          <button type="button" onClick={handler} className="gray" id="zero">0</button>
+          <button type="button" onClick={handler} className="gray">.</button>
+          <button type="button" onClick={handler} className="orange">=</button>
         </div>
       </div>
     </section>
